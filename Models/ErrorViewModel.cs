c627a1adbb41498649d5ad4c5151690a13636b8b
Thiago Rinaldi <68868756+thiagorinaldi@users.noreply.github.com>
2024-02-19 18:04:19 -0300

    Initial Deploy

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AceleraPlenoProjetoFinal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
