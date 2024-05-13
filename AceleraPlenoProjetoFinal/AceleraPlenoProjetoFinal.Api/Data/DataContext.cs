94c8d60daf83e206213c72d8d205ab4c06924ede
Thiago Rinaldi <68868756+thiagorinaldi@users.noreply.github.com>
2024-05-13 15:03:11 -0300

    Recriando repositório para aceitar o gitignore

---
+++ AceleraPlenoProjetoFinal/AceleraPlenoProjetoFinal.Api/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using AceleraPlenoProjetoFinal.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AceleraPlenoProjetoFinal.Api.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext() { }
+
+    public DataContext(DbContextOptions<DataContext> opt) : base(opt) { }
+
+    public DbSet<TransportadoraValoresModel> TransportadoraValores { get; set; }
+
+    public DbSet<UnidadeInstituicaoModel> UnidadeInstituicao { get; set; }
+
+    public DbSet<UnidadeInstituicaoTransportadoraValoresModel> UnidadeInstituicaoTransportadoraValores { get; set; }
+
+    public DbSet<TipoTerminalModel> TipoTerminal  { get; set; }
+
+    public DbSet<TipoOperacaoModel> TipoOperacao { get; set; }
+
+    public DbSet<OperacaoModel> Operacao { get; set; }
+}
